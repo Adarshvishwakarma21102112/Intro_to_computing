1d91909d7207e1b7e447427d50928625935b9e69
Adarshvishwakarma21102112 <104302881+Adarshvishwakarma21102112@users.noreply.github.com>
2022-04-25 23:53:49 +0530

    Add files via upload

---
+++ assignment-1/output.py
@@ -0,0 +1,42 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: C:\Users\harsh\Desktop\input.py ===================
+ans 1
+
+enter first number: 15
+enter second number: 20
+enter third number: 25
+20.0
+______________________________________________________________________
+
+ans 2
+
+enter gross income: 20000
+enter no. of dependents: 2
+800.0
+________________________________________________________________________
+
+ans 3
+
+Student[SID, Name, Gender, Course name, CGPA]
+Student= [21102112, 'Adarsh Vishwakarma', 'M', 'Civil', 8.5]
+______________________________________________________________________
+
+ans 4
+
+Marks of five students
+List before sorting [100, 55, 85, 67, 77]
+List after sorting [55, 67, 77, 85, 100]
+_______________________________________________________________________
+
+ans5
+
+
+(a)
+
+Colour ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+(b)
+
+Colour ['Red', 'Green', 'White', 'Purple', 'Yellow']
