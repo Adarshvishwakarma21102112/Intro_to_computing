7cd0adae886676f4e9c87115473ace572b0ace41
Adarshvishwakarma21102112 <104302881+Adarshvishwakarma21102112@users.noreply.github.com>
2022-05-07 00:05:18 +0530

    Add files via upload

---
+++ Assignment 2/assignment 2 output.py
@@ -0,0 +1,76 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:\Users\91873\Desktop\assignment 2 input.py ============
+ANS 1
+
+(a)length of input string is:
+ 35
+
+(b)string in reverse order is:
+ egaugnal evitisnes esac a si nohtyP
+
+(c)s1=
+ a case sensitive
+
+(d)new string after replacing:
+ Python is object orientedlanguage
+
+(e)The index of substring "a" is:
+ 10
+
+(f)input string with white spaces removed:
+ Pythonisacasesensitivelanguage
+ANS 2
+Please enter your name:
+Adarsh vishwakarma
+
+Please enter your SID:
+21102112
+
+Please enter your Department name:
+Civil
+
+Please enter your CGPA:
+7.5
+Hey, Adarsh vishwakarma Here!
+My SID is 21102112
+I am from Civil department and my CGPA is 7.5
+ANS 3
+
+(a)  a&b = 8
+
+(b) a|b = 58
+
+(c) a^b = 50
+
+(d)
+After left shift by 2-bits, a = 224
+After left shift by 2-bits, b = 40
+
+(e)
+After right shift by 2-bits, a= 14
+After right shift by 4-bits, b= 0
+Ans 4
+Enter the sentence/string -
+my name is adarsh vishwakarma
+
+Yes
+Ans 5
+
+Enter the 1st length:
+7
+
+Enter the 2nd length:
+4
+
+Enter the 3rd length:
+6
+
+ Yes,entered three lengths can form a triangle
+Ans6
+enter value of a:
+34
+enter value of b:
+45
+4
