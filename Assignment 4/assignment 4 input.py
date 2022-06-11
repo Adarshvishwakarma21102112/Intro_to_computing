e025513c5a9b5522e28de7f1762398aa977ed838
Adarshvishwakarma21102112 <104302881+Adarshvishwakarma21102112@users.noreply.github.com>
2022-06-12 01:53:58 +0530

    Add files via upload

---
+++ Assignment 4/assignment 4 input.py
@@ -0,0 +1,73 @@
+                              #Assignment 4
+#________________________________________________________________________________________________________________________________
+
+print("Ans 1")
+n=int(input("enter the marks:"))
+if n<25 :                                  #grade for below 25
+    print("correspinding grade","=","F")
+elif n>=25 and n<45 :                       #grade for 25 to 45
+   print("correspinding grade","=","E")
+elif n>=45 and n<50 :                       #grade for 45 to 50
+    print("correspinding grade","=","D")
+elif n>=50 and n<60 :                       #grade for 50 to 60
+    print("correspinding grade","=","C")
+elif n>=60 and n<80 :                       #grade for 60 to 80
+    print("correspinding grade","=","B")
+elif n>=80 and n<=100 :                     #grade for above 80 and max to 100
+    print("correspinding grade","=","A")
+else :                                       # grade for above 100 which is not defined
+    print("not defined")
+
+
+#____________________________________________________________________________________________________________________
+
+print("\nAns 2")
+                           #program to find year is leap year or not
+year = int(input("enter year:"))
+if year % 400 == 0 :         #if year is divisible by 400 then it is leap year
+    print(year, "is a Leap Year")
+elif year % 100 == 0 :        # year is divisible by only 100 then it is not leap year
+    print(year, "is not a Leap Year")
+elif year % 4 == 0 :          # year is divisible by 4 when it is not divisible by 100 
+    print(year, "is a Leap Year")
+else :
+    print(year, "is not a Leap Year")
+
+#_____________________________________________________________________________________________________________________________
+
+print("\nAns 3")
+                                #python program for multiplication game
+                            #import random
+import random            
+                                       #no of problems want
+gamenumber = int(input("How many probems do you want?\n"))  
+
+count = 1
+while count <= gamenumber:
+    num_1 = random.randint(0,9)       #taking randomly any no between 0 and 9 inclusively
+    num_2 = random.randint(0,9)
+    print("\nQuestion",count,":",num_1,"X",num_2)
+    guess = int(input())
+    answer =(num_1*num_2)
+                                     # when input answer is correct
+    if guess == answer:       
+        
+        print("Right!")
+    else:                             #when input anser is wrong
+        print("Wrong, the answer is", answer, ".")
+    count=count+1
+
+        
+#____________________________________________________________________________________________________________________________________
+
+print("\nAns 4")
+x=500                          #we know candies are less than 200
+for candies in range (x):                     #we use range function using for 
+    if candies%5==2:                 # if candies are divide by 5 reminder should be 2
+        if candies%6==3:             # if candies are divide by 6 reminder should be 3
+            if candies%7==2:         # if candies are divide by 7 reminder should be 2
+                print("no of candies present in the bowl","=",candies)
+                break
+                
+
+    
---
+++ Assignment 4/assignment 4 output.py
@@ -0,0 +1,58 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\91873\Desktop\assignment 4.py ===============
+Ans 1
+enter the marks:65
+correspinding grade = B
+
+Ans 2
+enter year:2014
+2014 is not a Leap Year
+
+Ans 3
+How many probems do you want?
+10
+
+Question 1 : 4 X 2
+8
+Right!
+
+Question 2 : 7 X 1
+7
+Right!
+
+Question 3 : 4 X 6
+24
+Right!
+
+Question 4 : 3 X 7
+20
+Wrong, the answer is 21 .
+
+Question 5 : 9 X 0
+6
+Wrong, the answer is 0 .
+
+Question 6 : 9 X 3
+27
+Right!
+
+Question 7 : 7 X 7
+49
+Right!
+
+Question 8 : 6 X 6
+32
+Wrong, the answer is 36 .
+
+Question 9 : 9 X 3
+27
+Right!
+
+Question 10 : 6 X 4
+24
+Right!
+
+Ans 4
+no of candies present in the bowl = 177
