b02997520a7bccc32db4cc8048fd58ae4cd1447a
Adarshvishwakarma21102112 <104302881+Adarshvishwakarma21102112@users.noreply.github.com>
2022-07-03 20:23:36 +0530

    Add files via upload

---
+++ Assignment 6/assignment 6 output.py
@@ -0,0 +1,59 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\91873\Desktop\Assignment python\Assignment 6\assignment 6 input.py
+Ans 1
+enter the number :128
+128 ,this is not a perfect number
+
+Ans 2
+enter the word :madam
+True
+
+Ans 3
+enter the no of rows :7
+       [1]
+      [1, 1]
+     [1, 2, 1]
+    [1, 3, 3, 1]
+   [1, 4, 6, 4, 1]
+  [1, 5, 10, 10, 5, 1]
+ [1, 6, 15, 20, 15, 6, 1]
+
+Ans 4
+enter the string :The quick brown fox jumps over the lazy dog
+yes,  The quick brown fox jumps over the lazy dog , this string is Pangram
+
+Ans 5
+enter the words: adarsh vicky joe robin kapil gazal rajat
+alphabetically arranged words:
+adarsh-gazal-joe-kapil-rajat-robin-vicky
+
+Ans 6
+
+Student ID: 21102112
+Student Name:  Adrsh vishwakarma
+
+Student ID: 21102110
+Student Name:  Adarsh vishwakarma
+
+Student Name: Adarsh vishwakarma
+Student Class:  Btech 1st year
+
+Ans 7
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Ans 8
+[[-10, 2, 8], [-7, -3, 10]]
+
+Ans 9
+True
+False
+True
